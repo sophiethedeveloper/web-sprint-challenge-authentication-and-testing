97fcf99864a9ffd1b01d898328931d84a5d0a609
sofia Luna <Sofia21luna@gmail.com>
2020-11-13 10:43:55 -0600

    registration is complete

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,36 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-router.post('/register', (req, res) => {
-  // implement registration
+const { isValid } = require("../jokes/jokes-service");
+// const { jwtSecret } = require("./secrets");
+const Users = require("../jokes/jokes-models");
+
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    const hash = bcrypt.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    Users.add(credentials)
+      .then((user) => {
+        res.status(201).json({ data: user });
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password",
+    });
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,26 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("./secrets");
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (!token) {
+    res.status(401).json({ you: "shall not pass!" });
+    return;
+  }
+
+  jwt.verify(token, jwtSecret, (err, decoded) => {
+    if (err) {
+      console.log("Decoded error -> ", err);
+      res.status(401).json({ message: "token is bad" });
+      return;
+    }
+  });
+
+  console.log("decoded token -> ", decoded);
+  req.decodedJwt = decoded;
+  next();
 };
