4cc0430d061491c45866ab409bc1be4c10c285b8
sofia Luna <Sofia21luna@gmail.com>
2020-11-13 10:52:42 -0600

    login is complete

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 const { isValid } = require("../jokes/jokes-service");
-// const { jwtSecret } = require("./secrets");
+const { jwtSecret } = require("./secrets");
 const Users = require("../jokes/jokes-models");
 
 router.post("/register", (req, res) => {
@@ -32,6 +32,42 @@ router.post("/register", (req, res) => {
 
 router.post("/login", (req, res) => {
   // implement login
+
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username })
+      .then(([user]) => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = makeToken(user);
+
+          res.status(200).json({ message: "welcome to dad jokes", token });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "Please provide username and password. Password should be alphanumeric",
+    });
+  }
 });
+
+function makeToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: "45 seconds",
+  };
+
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
